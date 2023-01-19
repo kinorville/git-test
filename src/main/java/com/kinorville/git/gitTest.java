a88c476af4ef0210f7f399178c78897b19bca72e
yjy <kinorville@163.com>
2023-01-19 20:11:24 +0800

    master commit

--- src/main/java/com/kinorville/git/gitTest.java
+++ src/main/java/com/kinorville/git/gitTest.java
@@ -2,6 +2,6 @@ package com.kinorville.git;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.println("hello 1");
+        System.out.println("master commit");
     }
 }
